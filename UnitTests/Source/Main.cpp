d2546b59d88c217ee98e32633d85e077d7887e5d
Janos Buttgereit <janos.buttgereit@fh-muenster.de>
2018-12-04 17:11:32 +0100

    Added a simple Unit Test runner application

---
+++ UnitTests/Source/Main.cpp
@@ -0,0 +1,40 @@
+/*
+This file is part of SoftwareDefinedRadio4JUCE.
+
+SoftwareDefinedRadio4JUCE is free software: you can redistribute it
+and/or modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+SoftwareDefinedRadio4JUCE is distributed in the hope that it will be
+useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with SoftwareDefinedRadio4JUCE. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+class SimpleUnitTestRunner : public UnitTestRunner
+{
+    void logMessage (const String& message) override
+    {
+        std::cout << message << std::endl;
+    }
+};
+
+//==============================================================================
+int main (int argc, char* argv[])
+{
+    SimpleUnitTestRunner runner;
+
+    runner.runAllTests();
+
+    for (int i = 0; i < runner.getNumResults(); ++i)
+        if (runner.getResult(i)->failures > 0)
+            return 1;
+
+    return 0;
+}
