29117fba3e0c865bd9b0a59d06f4c9ee42b9f516
Janos Buttgereit <janos.buttgereit@fh-muenster.de>
2018-12-10 15:49:52 +0100

    MCVWriter: Added functionality to dynamically write to an MCV file

--- ntlab_software_defined_radio/MCVFileFormat/MCVWriter.cpp
+++ ntlab_software_defined_radio/MCVFileFormat/MCVWriter.cpp
@@ -19,6 +19,189 @@ along with SoftwareDefinedRadio4JUCE. If not, see <http://www.gnu.org/licenses/>
 
 namespace ntlab
 {
+    MCVWriter::MCVWriter (int numChannels, bool useDoublePrecision, bool isComplex, juce::File& outputFile, int fifoSize)
+      : juce::AbstractFifo (fifoSize),
+        outputStream (outputFile)
+    {
+        jassert (outputFile.hasFileExtension ("mcv"));
+        jassert (numChannels > 0);
+
+        mcvWriterThread->newWriterCreated (this);
+
+        metadata.reset (new MCVHeader (isComplex, useDoublePrecision, numChannels));
+
+        auto numBytesPerFIFOChannel = metadata->sizeOfOneValue() * fifoSize;
+        fifoBuffers.reserve (static_cast<size_t> (numChannels));
+        tmpChannelPointers.resize (static_cast<size_t> (numChannels));
+
+        for (int i = 0; i < numChannels; ++i)
+            fifoBuffers.emplace_back (juce::MemoryBlock (numBytesPerFIFOChannel));
+
+        outputStream.setPosition (MCVHeader::sizeOfHeaderInBytes);
+    }
+
+    MCVWriter::~MCVWriter()
+    {
+        updateMetadataHeader();
+        mcvWriterThread->writerDeleted (this);
+    }
+
+    bool MCVWriter::isValid () {return outputStream.openedOk(); }
+
+    void MCVWriter::waitForEmptyFIFO (int timeout)
+    {
+        waitForEmptyFIFOEvent.wait (timeout);
+    }
+
+    void MCVWriter::updateMetadataHeader ()
+    {
+        std::lock_guard<std::mutex> scopedOutFileLock (outputFileLock);
+
+        auto outputStreamPosition = outputStream.getPosition();
+
+        metadata->setNumRowsOrSamples (numSamples);
+        metadata->writeToFile (outputStream);
+
+        outputStream.setPosition (outputStreamPosition);
+        outputStream.flush();
+    }
+
+    void MCVWriter::appendSampleBuffer (ntlab::SampleBufferReal<float>& bufferToAppend)
+    {
+        jassert (bufferToAppend.getNumChannels() == metadata->getNumColsOrChannels());
+        int numSamplesToWrite = bufferToAppend.getNumSamples();
+
+        if (numSamplesToWrite)
+            waitForEmptyFIFOEvent.reset();
+
+        int fifoStartIdx1, fifoBlockSize1, fifoStartIdx2, fifoBlockSize2;
+        prepareToWrite (numSamplesToWrite, fifoStartIdx1, fifoBlockSize1, fifoStartIdx2, fifoBlockSize2);
+
+        //todo extend sample buffer classes to make this possible
+        jassert (!metadata->isComplex());
+        jassert (!metadata->hasDoublePrecision());
+
+        if (fifoBlockSize1 > 0)
+        {
+            setTmpChannelPointersToIndex (fifoStartIdx1);
+            SampleBufferReal<float> fifoBlock1 (static_cast<int> (metadata->getNumColsOrChannels()), fifoBlockSize1, reinterpret_cast<float**> (tmpChannelPointers.data()));
+            bufferToAppend.copyTo (fifoBlock1, fifoBlockSize1, static_cast<int> (metadata->getNumColsOrChannels()));
+        }
+
+        if (fifoBlockSize2 > 0)
+        {
+            setTmpChannelPointersToIndex (fifoStartIdx2);
+            SampleBufferReal<float> fifoBlock2 (static_cast<int> (metadata->getNumColsOrChannels()), fifoBlockSize2, reinterpret_cast<float**> (tmpChannelPointers.data()));
+            bufferToAppend.copyTo (fifoBlock2, fifoBlockSize2, static_cast<int> (metadata->getNumColsOrChannels()), fifoBlockSize1);
+        }
+
+        finishedWrite (fifoBlockSize1 + fifoBlockSize2);
+
+        mcvWriterThread->notify();
+    }
+
+    void MCVWriter::appendSampleBuffer (ntlab::SampleBufferReal<double>& bufferToAppend)
+    {
+        jassert (bufferToAppend.getNumChannels() == metadata->getNumColsOrChannels());
+        int numSamplesToWrite = bufferToAppend.getNumSamples();
+
+        if (numSamplesToWrite)
+            waitForEmptyFIFOEvent.reset();
+
+        int fifoStartIdx1, fifoBlockSize1, fifoStartIdx2, fifoBlockSize2;
+        prepareToWrite (numSamplesToWrite, fifoStartIdx1, fifoBlockSize1, fifoStartIdx2, fifoBlockSize2);
+
+        //todo extend sample buffer classes to make this possible
+        jassert (!metadata->isComplex());
+        jassert (metadata->hasDoublePrecision());
+
+        if (fifoBlockSize1 > 0)
+        {
+            setTmpChannelPointersToIndex (fifoStartIdx1);
+            SampleBufferReal<double> fifoBlock1 (static_cast<int> (metadata->getNumColsOrChannels()), fifoBlockSize1, reinterpret_cast<double**> (tmpChannelPointers.data()));
+            bufferToAppend.copyTo (fifoBlock1, fifoBlockSize1, static_cast<int> (metadata->getNumColsOrChannels()));
+        }
+
+        if (fifoBlockSize2 > 0)
+        {
+            setTmpChannelPointersToIndex (fifoStartIdx2);
+            SampleBufferReal<double> fifoBlock2 (static_cast<int> (metadata->getNumColsOrChannels()), fifoBlockSize2, reinterpret_cast<double**> (tmpChannelPointers.data()));
+            bufferToAppend.copyTo (fifoBlock2, fifoBlockSize2, static_cast<int> (metadata->getNumColsOrChannels()), fifoBlockSize1);
+        }
+
+        finishedWrite (fifoBlockSize1 + fifoBlockSize2);
+
+        mcvWriterThread->notify();
+    }
+
+    void MCVWriter::appendSampleBuffer (ntlab::SampleBufferComplex<float>& bufferToAppend)
+    {
+        jassert (bufferToAppend.getNumChannels() == metadata->getNumColsOrChannels());
+        int numSamplesToWrite = bufferToAppend.getNumSamples();
+
+        if (numSamplesToWrite)
+            waitForEmptyFIFOEvent.reset();
+
+        int fifoStartIdx1, fifoBlockSize1, fifoStartIdx2, fifoBlockSize2;
+        prepareToWrite (numSamplesToWrite, fifoStartIdx1, fifoBlockSize1, fifoStartIdx2, fifoBlockSize2);
+
+        //todo extend sample buffer classes to make this possible
+        jassert (metadata->isComplex());
+        jassert (!metadata->hasDoublePrecision());
+
+        if (fifoBlockSize1 > 0)
+        {
+            setTmpChannelPointersToIndex (fifoStartIdx1);
+            SampleBufferComplex<float> fifoBlock1 (static_cast<int> (metadata->getNumColsOrChannels()), fifoBlockSize1, reinterpret_cast<std::complex<float>**> (tmpChannelPointers.data()));
+            bufferToAppend.copyTo (fifoBlock1, fifoBlockSize1, static_cast<int> (metadata->getNumColsOrChannels()));
+        }
+
+        if (fifoBlockSize2 > 0)
+        {
+            setTmpChannelPointersToIndex (fifoStartIdx2);
+            SampleBufferComplex<float> fifoBlock2 (static_cast<int> (metadata->getNumColsOrChannels()), fifoBlockSize2, reinterpret_cast<std::complex<float>**> (tmpChannelPointers.data()));
+            bufferToAppend.copyTo (fifoBlock2, fifoBlockSize2, static_cast<int> (metadata->getNumColsOrChannels()), fifoBlockSize1);
+        }
+
+        finishedWrite (fifoBlockSize1 + fifoBlockSize2);
+
+        mcvWriterThread->notify();
+    }
+
+    void MCVWriter::appendSampleBuffer (ntlab::SampleBufferComplex<double>& bufferToAppend)
+    {
+        jassert (bufferToAppend.getNumChannels() == metadata->getNumColsOrChannels());
+        int numSamplesToWrite = bufferToAppend.getNumSamples();
+
+        if (numSamplesToWrite)
+            waitForEmptyFIFOEvent.reset();
+
+        int fifoStartIdx1, fifoBlockSize1, fifoStartIdx2, fifoBlockSize2;
+        prepareToWrite (numSamplesToWrite, fifoStartIdx1, fifoBlockSize1, fifoStartIdx2, fifoBlockSize2);
+
+        //todo extend sample buffer classes to make this possible
+        jassert (metadata->isComplex());
+        jassert (metadata->hasDoublePrecision());
+
+        if (fifoBlockSize1 > 0)
+        {
+            setTmpChannelPointersToIndex (fifoStartIdx1);
+            SampleBufferComplex<double> fifoBlock1 (static_cast<int> (metadata->getNumColsOrChannels()), fifoBlockSize1, reinterpret_cast<std::complex<double>**> (tmpChannelPointers.data()));
+            bufferToAppend.copyTo (fifoBlock1, fifoBlockSize1, static_cast<int> (metadata->getNumColsOrChannels()));
+        }
+
+        if (fifoBlockSize2 > 0)
+        {
+            setTmpChannelPointersToIndex (fifoStartIdx2);
+            SampleBufferComplex<double> fifoBlock2 (static_cast<int> (metadata->getNumColsOrChannels()), fifoBlockSize2, reinterpret_cast<std::complex<double>**> (tmpChannelPointers.data()));
+            bufferToAppend.copyTo (fifoBlock2, fifoBlockSize2, static_cast<int> (metadata->getNumColsOrChannels()), fifoBlockSize1);
+        }
+
+        finishedWrite (fifoBlockSize1 + fifoBlockSize2);
+
+        mcvWriterThread->notify();
+    }
+
     bool MCVWriter::writeSampleBuffer (ntlab::SampleBufferReal<float>& bufferToWrite, juce::File& outputFile)
     {
         return writeRawArray (bufferToWrite.getArrayOfReadPointers(), bufferToWrite.getNumChannels(), bufferToWrite.getNumSamples(), outputFile);
@@ -57,6 +240,95 @@ namespace ntlab
     bool MCVWriter::writeRawArray (const std::complex<double>** rawArray, int64_t numColsOrChannels, int64_t numRowsOrSamples, juce::File& outputFile)
     {
         return writeRaw (reinterpret_cast<const void**> (rawArray), numColsOrChannels, numRowsOrSamples, true, true, outputFile);
+    }
+
+    MCVWriter::MCVWriterThread::MCVWriterThread () : juce::Thread ("MCVWriter Thread")
+    {
+        // give it a slightly higher priority than the default one
+        startThread (6);
+    }
+
+    MCVWriter::MCVWriterThread::~MCVWriterThread ()
+    {
+        stopThread (100);
+    }
+
+    void MCVWriter::MCVWriterThread::newWriterCreated (ntlab::MCVWriter* newWriter)
+    {
+        writers.add (newWriter);
+    }
+
+    void MCVWriter::MCVWriterThread::writerDeleted (ntlab::MCVWriter* writerThatHasBeenDeleted)
+    {
+        auto idx = writers.indexOf (writerThatHasBeenDeleted);
+        writers.remove (idx);
+    }
+
+    void MCVWriter::MCVWriterThread::run ()
+    {
+        while (!threadShouldExit())
+        {
+            for (auto writer : writers)
+            {
+                const int numSamplesToWrite = writer->getNumReady();
+
+                if (numSamplesToWrite == 0)
+                {
+                    writer->waitForEmptyFIFOEvent.signal();
+                    continue;
+                }
+
+
+                int fifoStartIdx1, fifoBlockSize1, fifoStartIdx2, fifoBlockSize2;
+                writer->prepareToRead (numSamplesToWrite, fifoStartIdx1, fifoBlockSize1, fifoStartIdx2, fifoBlockSize2);
+
+                const auto numChannelsToWrite = writer->metadata->getNumColsOrChannels();
+                const auto numBytesPerValue = writer->metadata->sizeOfOneValue();
+                auto& outputStream = writer->outputStream;
+
+                std::lock_guard<std::mutex> scopedOutFileLock (writer->outputFileLock);
+
+                if (fifoBlockSize1 > 0)
+                {
+                    for (int s = 0; s < fifoBlockSize1; ++ s)
+                    {
+                        for (int c = 0; c < numChannelsToWrite; ++ c)
+                        {
+                            void* valueToWrite = juce::addBytesToPointer (writer->fifoBuffers[c].getData(), numBytesPerValue * (fifoStartIdx1 +s));
+                            outputStream.write (valueToWrite, numBytesPerValue);
+                        }
+                    }
+                    writer->numSamples += fifoBlockSize1;
+                }
+
+                if (fifoBlockSize2 > 0)
+                {
+                    for (int s = 0; s < fifoBlockSize2; ++ s)
+                    {
+                        for (int c = 0; c < numChannelsToWrite; ++ c)
+                        {
+                            void* valueToWrite = juce::addBytesToPointer (writer->fifoBuffers[c].getData(), numBytesPerValue * (fifoStartIdx2 +s));
+                            outputStream.write (valueToWrite, numBytesPerValue);
+                        }
+                    }
+                    writer->numSamples += fifoBlockSize2;
+                }
+
+                writer->finishedRead (fifoBlockSize1 + fifoBlockSize2);
+
+                if (writer->getNumReady() == 0)
+                    writer->waitForEmptyFIFOEvent.signal();
+            }
+
+            wait (-1);
+        }
+    }
+
+    void MCVWriter::setTmpChannelPointersToIndex (int index)
+    {
+        auto numBytesPerValue = metadata->sizeOfOneValue();
+        for (int i = 0; i < metadata->getNumColsOrChannels(); ++i)
+            tmpChannelPointers[i] = juce::addBytesToPointer (fifoBuffers[i].getData(), index * numBytesPerValue);
     }
 
     bool MCVWriter::writeRaw (const void** rawArray, int64_t numColsOrChannels, int64_t numRowsOrSamples, bool isComplex, bool isDouble, juce::File& outputFile)
--- ntlab_software_defined_radio/MCVFileFormat/MCVWriter.h
+++ ntlab_software_defined_radio/MCVFileFormat/MCVWriter.h
@@ -28,10 +28,47 @@ namespace ntlab
      * possibility to dynamically write to an MCV file. For more info on the MCV format refer to the Readme file in the
      * MCVFileFormat folder in the root folder of this repo.
      */
-    class MCVWriter
+    class MCVWriter : private juce::AbstractFifo
     {
+        friend class MCVWriterThread;
     public:
 
+        /**
+         * Create an MCVWriter instance if you want to continously write and append samples to a MCV File. In case you
+         * want to write a matrix or a sample buffer at once, use on of the static member functions
+         */
+        MCVWriter (int numChannels, bool useDoublePrecision, bool isComplex, juce::File& outputFile, int fifoSize = 8192);
+
+        ~MCVWriter();
+
+        /** Returns true if the writer could successfully open the output file and is ready to write */
+        bool isValid();
+
+        /**
+         * This updates the metadata header at the beginning of the file so that it stays valid. This is done
+         * automatically in the destructor, however calling this in between makes keeps the file valid before
+         * deleting the writer.
+         */
+        void updateMetadataHeader();
+
+        /**
+         * As all writes through the appendSampleBuffer functions are written to a FIFO first and then actually written
+         * to disk by a dedicated writer thread. Calling this function will lead to wait for the thread to finalize all
+         * disk writing until the fifo is empty.
+         */
+        void waitForEmptyFIFO (int timeout = -1);
+
+        /** Appends the content of this sample buffer to the file */
+        void appendSampleBuffer (SampleBufferReal<float>& bufferToAppend);
+
+        /** Appends the content of this sample buffer to the file */
+        void appendSampleBuffer (SampleBufferReal<double>& bufferToAppend);
+
+        /** Appends the content of this sample buffer to the file */
+        void appendSampleBuffer (SampleBufferComplex<float>& bufferToAppend);
+
+        /** Appends the content of this sample buffer to the file */
+        void appendSampleBuffer (SampleBufferComplex<double>& bufferToAppend);
 
         /** Writes a complete SampleBufferReal<float> to the desired output file */
         static bool writeSampleBuffer (SampleBufferReal<float>& bufferToWrite, juce::File& outputFile);
@@ -59,7 +96,33 @@ namespace ntlab
 
     private:
 
-        static bool writeRaw (const void** rawArray, int64_t numColsOrChannels, int64_t numRowsOrSamples, bool isComplex, bool isDouble, juce::File& outputFile);
+        class MCVWriterThread : public juce::Thread
+        {
+        public:
+            MCVWriterThread();
+            ~MCVWriterThread();
 
+            void newWriterCreated (MCVWriter* newWriter);
+            void writerDeleted (MCVWriter* writerThatHasBeenDeleted);
+
+            void run() override;
+        private:
+            juce::Array<MCVWriter*, juce::SpinLock> writers;
+        };
+
+        juce::SharedResourcePointer<MCVWriterThread> mcvWriterThread;
+        juce::WaitableEvent waitForEmptyFIFOEvent;
+
+        std::vector<juce::MemoryBlock> fifoBuffers;
+        std::vector<void*> tmpChannelPointers;
+
+        juce::FileOutputStream outputStream;
+        std::mutex outputFileLock;
+        std::unique_ptr<MCVHeader> metadata;
+        int numSamples = 0;
+
+        void setTmpChannelPointersToIndex (int index);
+
+        static bool writeRaw (const void** rawArray, int64_t numColsOrChannels, int64_t numRowsOrSamples, bool isComplex, bool isDouble, juce::File& outputFile);
     };
 }
