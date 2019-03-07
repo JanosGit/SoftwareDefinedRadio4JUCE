1bd642b874a5aea666f2b886ce405acefb3dc286
Janos Buttgereit <janos.buttgereit@fh-muenster.de>
2019-03-07 16:09:40 +0100

    Added missing .mm file in module root folder for Apple compatibility

---
+++ ntlab_software_defined_radio/ntlab_software_defined_radio.mm
@@ -0,0 +1,18 @@
+/*
+This file is part of SoftwareDefinedRadio4JUCE.
+
+SoftwareDefinedRadio4JUCE is free software: you can redistribute it
+and/or modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+SoftwareDefinedRadio4JUCE is distributed in the hope that it will be
+useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with SoftwareDefinedRadio4JUCE. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "ntlab_software_defined_radio.cpp"
