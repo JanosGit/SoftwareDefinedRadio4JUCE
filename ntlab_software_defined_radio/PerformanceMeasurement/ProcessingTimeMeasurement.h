161e5071e39acb538792249eb604ef409126817b
NT-Lab <janos.buttgereit@fh-muenster.de>
2019-09-30 15:03:39 +0200

    Added LightweightProcessingTimeMeasurement

--- ntlab_software_defined_radio/PerformanceMeasurement/ProcessingTimeMeasurement.h
+++ ntlab_software_defined_radio/PerformanceMeasurement/ProcessingTimeMeasurement.h
@@ -153,4 +153,89 @@ namespace ntlab
             resultCallback (timePerSample, percentsOfProcessingTimeUsedPerSample, currentNumSamps);
         }
     };
+
+    class LightweightProcessingTimeMeasurement
+    {
+    public:
+
+        /**
+         * The object that helps recording the time spent in the processing callback. You cannot create one directly,
+         * use startScopedMeasurement instead.
+         */
+        class ScopedProcessingTimeMeasurement
+        {
+            friend class LightweightProcessingTimeMeasurement;
+        public:
+            ~ScopedProcessingTimeMeasurement()
+            {
+                auto delta = juce::Time::getHighResolutionTicks() - startTime;
+
+                parent.ticks    += delta;
+                parent.numSamps += numSamples;
+            }
+
+        private:
+            LightweightProcessingTimeMeasurement& parent;
+            const int numSamples;
+            const juce::int64 startTime;
+
+            ScopedProcessingTimeMeasurement (LightweightProcessingTimeMeasurement& parentInstance, const int numSamplesThisBlock)
+                    : parent     (parentInstance),
+                      numSamples (numSamplesThisBlock),
+                      startTime  (juce::Time::getHighResolutionTicks()) {}
+        };
+
+        using MeasurementResultCallback = std::function<void (double microSecondsPerSample, double percentsOfProcessingTimeUsedPerSample, juce::int64 numSamplesAveraged)>;
+
+        /**
+         * Creates a ProcessingTimeMeasurement that roughly invokes the measurement result callback after the desired
+         * number of samples has been processed. The default callback will print the results to the current juce::Logger,
+         * you can alternatively pass in a custom callback. The callback will always be invoked from a thread owned by
+         * this class.
+         */
+        LightweightProcessingTimeMeasurement (MeasurementResultCallback callback = [] (double us, double load, juce::int64)
+                                              {
+                                                 juce::Logger::writeToLog ("Processing time measurement results: " + juce::String (us) + "μs per sample, " + juce::String (load) + "% load");
+                                              })
+                : resultCallback (callback) {}
+
+        /** Always call this before starting to stream */
+        void processingStarts (double sampleRate)
+        {
+            availableProcessingTimePerSampleMicroSeconds = 1e6 / sampleRate;
+        }
+
+        /** Always call this afer streaming has stopped */
+        void processingEnds()
+        {
+            invokeResultCallback (ticks, numSamps);
+            ticks = 0;
+            numSamps = 0;
+        }
+
+        /**
+         * This creates a ScopedProcessingTimeMeasurement that will record the time spent in the callback.
+         * Create one on the stack just before the desired part of the processing you want to process and let it go
+         * out of scope after the desired part of the processing is over
+         */
+        const ScopedProcessingTimeMeasurement startScopedMeasurement (const int numSamplesThisBlock)
+        {
+            return ScopedProcessingTimeMeasurement (*this, numSamplesThisBlock);
+        }
+
+    private:
+        juce::int64               ticks = 0, numSamps = 0;
+        MeasurementResultCallback resultCallback;
+        double                    availableProcessingTimePerSampleMicroSeconds = 0;
+
+        void invokeResultCallback (juce::int64 currentTicks, juce::int64 currentNumSamps)
+        {
+            auto timeInMicroSeconds = juce::Time::highResolutionTicksToSeconds (currentTicks) * 1e6;
+            auto timePerSample = timeInMicroSeconds / currentNumSamps;
+
+            auto percentsOfProcessingTimeUsedPerSample = (timePerSample / availableProcessingTimePerSampleMicroSeconds) * 100;
+
+            resultCallback (timePerSample, percentsOfProcessingTimeUsedPerSample, currentNumSamps);
+        }
+    };
 }
